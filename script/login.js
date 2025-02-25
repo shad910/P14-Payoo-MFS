c25dcc13fb171c612bb3891aac411d33b686fed6
shad910 <shadshs91@gmail.com>
2025-02-26 02:16:40 +0600

    first update

---
+++ script/login.js
@@ -0,0 +1,23 @@
+const loginBTN = document.getElementById("login-btn").addEventListener("click", function(event){
+    event.preventDefault();
+    const accountNumber = document.getElementById("account-number").value;
+    const pinNumber = document.getElementById("pin-number").value;
+
+    const convertPinNumber = parseInt(pinNumber, 10);
+
+    console.log(typeof accountNumber);
+    console.log(typeof convertPinNumber);
+
+
+    if( accountNumber.length === 11 ){
+        if( pinNumber.length > 3 || pinNumber.length === 4 ){
+            window.location.href='../main.html';
+        }
+        else{
+            alert('Enter valid password')
+        }
+    }
+    else{
+        alert('Enter valid number')
+    }
+})
---
+++ script/logout.js
@@ -0,0 +1,4 @@
+const logout = document.getElementById("logout-btn");
+logout.addEventListener("click",function(){
+    window.location.href='../index.html'
+})
